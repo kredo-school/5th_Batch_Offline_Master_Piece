4366023f69273072e87d50fda5cd2840945d1ac8
naotaro123 <ponpokoman05@gmail.com>
2024-09-24 14:13:36 +0800

    I made books model and book migration.

---
+++ database/migrations/2024_09_23_091059_create_books_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+        $table->id(); 
+        $table->string('title'); 
+        $table->text('description');
+        $table->date('publication_date'); 
+        $table->string('publisher'); 
+        $table->string('isbn_code')->unique(); 
+        $table->decimal('price', 8, 2); 
+        $table->longText('image');
+        $table->timestamp('deleted_at')->nullable();
+        });
+    }
+    // up はmigrateした時に行われる動作
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+
+    //ロールバックした時に行われる動作　down
+};
