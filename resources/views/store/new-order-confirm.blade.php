1a3a06077275bd407e337a9e31f0c685b1903a8d
shoki <bokusulaumu@gmail.com>
2024-09-26 15:44:31 +0800

    store-new-order-confirm

---
+++ resources/views/store/new-order-confirm.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.app')
+
+@section('title','new-order-confirm')
+
+@section('content')
+<div class="row justify-content-center">
+    <div class="col-8 mt-1">
+        <div class="bg-white rounded my-5 px-5 overflow-auto profile-list"  style="height: 1100px">
+            <h2 class="h1 fw-bold text-grey mt-3">Confirm and Add New Order Books</h2><br>
+
+            @for ($i = 0; $i < 3; $i++)
+            <div class="row mt-4"><br>
+                    <p class="text-muted">Sep.12.2024</p>
+                    <div class="col-3">
+                        <img src="{{ asset('images/649634.png') }}" alt="$book->id" class="w-100 shadow search-list-img">
+                    </div>
+                    <div class="col-6 fs-32">
+                        <p>
+                            <p class="fs-32">$book->name</p>
+                            <p class="h4">$book->author->name</p>
+                            <i class="fa-solid fa-star text-warning"></i>
+                            <i class="fa-solid fa-star text-warning"></i>
+                            <i class="fa-solid fa-star text-warning"></i>
+                            <i class="fa-solid fa-star text-warning"></i>
+                            <i class="fa-regular fa-star text-warning"></i>
+                            4.2/5.0
+                        </p>
+                        <p class="text-danger fs-32 mt-5">¥23,000</p>
+                    </div>
+                    <div class="col-3 pb-0 pt-5 mt-5 mb-0">
+                        <div class="text-end w-75">
+                            <form action="" method="post">
+                                @csrf
+                                @method('DELETE')
+            
+                                <p class="text-start mb-0">Quantity</p>
+                                <input type="number" name="quantity" id="quantity" placeholder="Qauntity" class="form-control mb-4 w-100 text-center d-inline" value="1">
+                                <input type="submit" value="Delete" class="btn btn-danger w-100 mt-5 mb-0">
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <hr>
+            @endfor
+        </div>
+        <div class="text-end my-5 ">
+            <button type="submit" class="me-3 p-3 border-0 rounded new-order-confirm-proceed">Proceed to Next</button>
+        </div>
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\BookController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\StoreController;
 
 // Route::get('/', function () {
 //     return view('welcome');
@@ -15,6 +16,7 @@ Auth::routes();
 Route::group(['middleware' => 'auth'],function(){
     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
+Route::group(['prefix'=>'guest','as'=>'guest.'],function(){
     Route::group(['prefix'=>'profile','as'=>'profile.'],function(){
         Route::get('/show',[ProfileController::class,'show'])->name('show');
         Route::get('/bookmark',[ProfileController::class,'bookmark'])->name('bookmark');
@@ -23,16 +25,33 @@ Route::group(['middleware' => 'auth'],function(){
         Route::get('/searchlist',[ProfileController::class,'searchlist'])->name('searchlist');
     });
 
+    Route::get('/suggestion',[AuthorController::class, 'index']);
+
+    Route::group(['prefix'=>'thread','as'=>'thread.'],function(){
+
+
+        
+    });
+
+
+
+
+});
+
+
     Route::group(['prefix' => 'order', 'as' => 'order.'], function(){
         Route::get('/show', [BookController::class, 'show'])->name('show');
         Route::get('/upload', [HomeController::class, 'uploadImage'])->name('uploadImage');
     });
 
-    Route::get('/suggestion',[AuthorController::class, 'index']);
 
     Route::group(['prefix' => 'order', 'as' => 'order.'], function(){
         Route::get('/show', [BookController::class, 'show'])->name('show');
         Route::get('/confirm', [BookController::class, 'confirm'])->name('confirm');
+    });
+
+    Route::group(['prefix' => 'store', 'as' => 'store.'], function(){
+        Route::get('/show', [StoreController::class, 'newOrderConfirm'])->name('newOrderConfirm');
     });
 });
 
