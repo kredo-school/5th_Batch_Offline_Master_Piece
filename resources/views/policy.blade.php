ecf8987c715a7e6a50252f22f22dbf81ea05b607
Nanami Hata <>
2024-10-07 17:13:16 +0800

    no message

---
+++ resources/views/policy.blade.php
@@ -0,0 +1,96 @@
+@extends('layouts.app')
+
+@section('title', 'Our Policy')
+
+@section('content')
+
+<div class="container">
+    <div class="bg-white shadow my-5 rounded mx-auto" style="width: 70%; height: auto; padding-bottom: 8%;">
+        <img src="http://localhost:8000/images/logo2.png" alt="MasterPece logo" class="d-block mx-auto pt-5" style="width:12%;">
+        <h1 class="fw-bold text-center p-3">
+            Our Policy
+        </h1>
+        <div class="mx-5 m-5">
+            <p class="h3 my-4 mt-0">About persoal information</p>
+                <ol>
+                    <li>Information We Collect
+                        <br>We may collect the following personal information when you use our online servce:
+                        <p>
+                            </p><ul>
+                                <li>Name</li>
+                                <li>Email</li>
+                                <li>Address</li>
+                                <li>Phone number</li>
+                                <li>Birth date</li>
+                            </ul>
+                        <p></p>
+                    </li>
+                    <li>Purpose of Using Personal Information
+                        <p>
+                            </p><ul>
+                                <li>Order processing and confirmation</li>
+                                <li>Communication regarding orders</li>
+                                <li>Recommendations based on purchase history</li>
+                                <li>Research and analysis for service improvement"</li>
+                                <li>Customer support</li>
+                            </ul>
+                        <p></p>
+                    </li>
+                    <li>Security Management of Information
+                        <p>We take reasonable security measures to protect your personal information from unauthorized access, loss, destruction, or alteration.</p>
+                    </li>
+                    <li>Customer Rights
+                        <p>You have the following rights concerning your personal information:
+                            </p><ul>
+                                <li>Right to access and correct personal information</li>
+                                <li>Right to request deletion of information</li>
+                                <li>Right to data portability (the right to transfer your data to other services) To exercise these rights, please contact our customer support."</li>
+                            </ul>
+                        <p></p>
+                    </li>
+                </ol>
+            <p class="h3 my-4">About terms of service</p>
+            <ol>
+                <li>Introduction and Acceptance of Terms
+                    <p>By accessing or using our services, you agree to be bound by these Terms of Service. If you do not agree with any part of these terms, you may not use our services</p>
+                </li>
+                <li>User Responsibilities
+                    <p>Users agree not to use the service for any unlawful or prohibited activities, including but not limited to hacking, distributing malware, or violating any local, national, or international laws.</p>
+                </li>
+                <li>Account Registration and Security
+                    <p>You must provide accurate and current information when creating an account. You are responsible for maintaining the confidentiality of your account login information and for all activities that occur under your account.</p>
+                </li>
+                <li>Intellectual Property Rights
+                    <p>All content provided on this site, including text, images, graphics, logos, and software, is the property of Kredo IT abroad:MasterPiece and is protected by copyright, trademark, and other laws.</p>
+                </li>
+                <li> Termination of Service
+                    <p>We reserve the right to suspend or terminate your account at any time, without notice, if we determine that you have violated these terms.</p>
+                </li>
+                <li>Privacy Policy
+                    <p>Your use of the service is also governed by our Privacy Policy, which describes how we collect, use, and protect your information.</p>
+                </li>
+                <li>User-Generated Content
+                    <p>By submitting content to our platform, you grant Kredo IT abroad:MasterPiece a worldwide, non-exclusive, royalty-free license to use, distribute, and display your content.</p>
+                </li>
+            </ol>
+
+            <p class="h3 my-4">About advertising policy</p>
+            <p>Our website may display advertisements to users. These ads are provided by third-party advertising partners, and we do not control the content of these ads. Please note that clicking on an advertisement may redirect you to an external website, which may have its own privacy policies and terms of service. We are not responsible for the practices of these third-party sites.</p>
+            <p class="h3 my-4">Our goal</p>
+            <ul>
+                <li>For all book lovers
+                    <p class="p-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem atque enim assumenda. Ullam tempore asperiores beatae saepe quae, sed et odit quod similique voluptatum, mollitia illo! Obcaecati similique tenetur sunt!</p>
+                </li>
+                <li>For the future of book stores
+                    <p class="p-2">Lorem ipsum dolor sit amet consectetur, adipisicing elit. A aspernatur modi enim, reprehenderit quibusdam veniam eveniet iste fugit libero in. Reprehenderit, quae consequuntur incidunt consectetur voluptatem illum recusandae nisi rem?</p>
+                </li>
+                <li>For the workers engaging book stores.
+                    <p class="p-2">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut, odio? Iure consequuntur unde, non adipisci ipsum totam eum ratione, amet magni temporibus maxime id laboriosam quod nesciunt exercitationem! Quaerat, quae.</p>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+
+@endsection
