f6168b3b6304a4687a77891c96ffa118aebac755
Takuma Mizuno <petit09petit09@gmail.com>
2024-10-08 15:07:47 +0800

    I created like -mcr

---
+++ database/migrations/2024_10_08_070146_create_likes_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('likes', function (Blueprint $table) {
+            $table->unsignedBigInteger('guest_id');
+            $table->unsignedBigInteger('review_id');
+
+            $table->foreign('guest_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('review_id')->references('id')->on('reviews')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('likes');
+    }
+};
