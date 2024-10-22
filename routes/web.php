6a2676a9788aeedf1fa03a41bcd19783ff7caabf
Takuma Mizuno <petit09petit09@gmail.com>
2024-10-22 15:01:42 +0800

    I corrected error of profile pages

--- routes/web.php
+++ routes/web.php
@@ -31,7 +31,7 @@ Route::group(['middleware' => 'auth'], function () {
     Route::get('/welcome',[ProfileController::class,'welcome'])->name(name: 'welcome')->middleware('profile');
     Route::group(['prefix' => 'guest'], function () {
         Route::get('/policy', [HomeController::class, 'policy'])->name('policy');
-        Route::get('/home/second', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+        Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
         Route::group(['prefix'=>'profile','as'=>'profile.'],function(){
             Route::get('/{id}/show',[ProfileController::class,'show'])->name('show');
             Route::get('/{id}/bookmark',[ProfileController::class,'bookmark'])->name('bookmark');
